ff7a0ecbba791399f9131f934174a1ee05c1bf3d
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-08-21 14:06:22 +0300

    added header footer navbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponentComponent } from './quote-component/quote-component.component';
+import { QuoteDetailsComponentComponent } from './quote-component/quote-details-component/quote-details-component.component';
+import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponentComponent,
+    QuoteDetailsComponentComponent,
+    NavbarComponentComponent,
+    FooterComponentComponent,
+    HeaderComponentComponent
   ],
   imports: [
     BrowserModule,
