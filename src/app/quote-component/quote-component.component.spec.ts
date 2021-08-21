4f1bd7f1d29fc3a0070e97e2e2e576588b8acd92
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-08-21 14:00:57 +0300

    added quote components

---
+++ src/app/quote-component/quote-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuoteComponentComponent } from './quote-component.component';
+
+describe('QuoteComponentComponent', () => {
+  let component: QuoteComponentComponent;
+  let fixture: ComponentFixture<QuoteComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ QuoteComponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuoteComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/quote-component/quote-details-component/quote-details-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-details-component',
+  templateUrl: './quote-details-component.component.html',
+  styleUrls: ['./quote-details-component.component.scss']
+})
+export class QuoteDetailsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
