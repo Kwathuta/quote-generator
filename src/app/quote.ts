ea710abcd9210b340fc7b204e0917d5ec916df03
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-08-21 14:35:52 +0300

    defined quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,10 @@
 export class Quote {
+    datePosted:Date
+    likes:number
+    dislikes:number
+    constructor(public proverb:string, public user:string, public author:string) {
+        this.datePosted = new Date()
+        this.likes = 0
+        this.dislikes = 0
+    }
 }
