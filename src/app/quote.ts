f62f07dbed4625b1c189acd6155755f379d0de4d
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-08-24 14:54:02 +0300

    changed favicon

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,8 +1,8 @@
 export class Quote {
-    datePosted:Date
-    likes:number
-    dislikes:number
-    constructor(public proverb:string, public user:string, public author:string) {
+    datePosted: Date
+    likes: number
+    dislikes: number
+    constructor(public proverb: string, public user: string, public author: string) {
         this.datePosted = new Date()
         this.likes = 0
         this.dislikes = 0
